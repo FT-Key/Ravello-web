05bfea703bc9803e35e20a551702beb7f15fc65d
FT-Key <fr4nc0t2@gmail.com>
2025-10-31 14:52:58 -0300

    merge conflict

--- ravello-frontend/tailwind.config.js
+++ ravello-frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require("flowbite/plugin")],
+}
